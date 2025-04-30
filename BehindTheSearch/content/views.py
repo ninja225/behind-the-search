613edfdb8ba9d95b2924c1bc79824195c40669e6
unknown <mazenosama987654321@gmail.com>
2025-04-30 17:59:47 +0300

    content app

---
+++ BehindTheSearch/content/views.py
@@ -0,0 +1,42 @@
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import CourseVideo
+from .forms import CourseVideoForm
+
+def video_list(request):
+    videos = CourseVideo.objects.all()
+    return render(request, 'content/video_list.html', {'videos': videos})
+
+def video_detail(request, id):
+    video = get_object_or_404(CourseVideo, id=id) 
+    return render(request, 'content/video_detail.html', {'video': video})
+
+def create_course_video(request):
+    if request.method == 'POST':
+        form = CourseVideoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('video_list')
+    else:
+        form = CourseVideoForm()
+
+    return render(request, 'content/create_course_video.html', {'form': form})
+
+def video_description(request, video_id):
+    video = get_object_or_404(CourseVideo, id=video_id)
+    
+    return render(request, 'content/video_description.html', {'video': video})
+
+
+def edit_course_video(request, video_id):
+    video = get_object_or_404(CourseVideo, id=video_id)
+    
+    if request.method == 'POST':
+        form = CourseVideoForm(request.POST, request.FILES, instance=video)
+        if form.is_valid():
+            form.save()
+            return redirect('video_list')
+    else:
+        form = CourseVideoForm(instance=video)
+    
+    return render(request, 'content/edit_course_video.html', {'form': form, 'video': video})
