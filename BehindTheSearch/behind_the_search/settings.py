510daad2747e93014838c30cbf15911c68b456da
Dr-Ninja <marketkoo@mail.ru>
2025-05-01 03:36:12 +0300

    init static

--- BehindTheSearch/behind_the_search/settings.py
+++ BehindTheSearch/behind_the_search/settings.py
@@ -126,6 +126,7 @@ EMAIL_HOST_PASSWORD = 'okxp cjhf fyoh enfe'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
