6496364cc6b398be297668c398fb98f08a045e84
unknown <mazenosama987654321@gmail.com>
2025-05-03 01:50:44 +0300

    notification search

--- BehindTheSearch/content/urls.py
+++ BehindTheSearch/content/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('videos/', views.video_list, name='video_list'),
     path('video/<int:id>/', views.video_detail, name='video_detail'),
+    path('videos/stream/<int:id>/', views.stream_video, name='stream_video'),
     path('video/create/', views.create_course_video, name='create_course_video'),
     path('video/edit/<int:video_id>/', views.edit_course_video, name='edit_course_video'),
+
 ]
--- BehindTheSearch/notifications/views.py
+++ BehindTheSearch/notifications/views.py
@@ -4,10 +4,22 @@ from .models import Notification
 from django.core.mail import send_mail
 from django.conf import settings
 from adminBoard.decorators import superuser_required
-@superuser_required
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Notification
+
 def notification_list(request):
-    notifications = request.user.notifications.all().order_by('-created_at')
-    return render(request, 'notifications/list.html', {'notifications': notifications})
+    search_query = request.GET.get('search', '')
+    notifications = Notification.objects.all()
+
+    if search_query:
+        notifications = notifications.filter(from_user__username__icontains=search_query)
+
+    return render(request, 'notifications/list.html', {
+        'notifications': notifications,
+        'search_query': search_query
+    })
+
 
 @superuser_required
 def accept_request(request, notification_id):
