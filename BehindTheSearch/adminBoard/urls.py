03bb8bd74ff42bfad71536419b16ca6bd75d850d
Mazen osama <140162746+mazenosama493@users.noreply.github.com>
2025-05-03 23:23:00 +0300

    Delete BehindTheSearch directory

--- BehindTheSearch/adminBoard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='admin-dashboard'),
-    path('users', views.getUsers, name='users'),
-    path('user/<str:pk>/', views.getUser, name='user'),
-]
--- BehindTheSearch/content/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CourseVideo
-
-class CourseVideoForm(forms.ModelForm):
-    class Meta:
-        model = CourseVideo
-        fields = ['title', 'description', 'video','lesson_number']
