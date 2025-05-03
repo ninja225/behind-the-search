dff689ad819ca59283b9b3dfc598c45b694ab818
unknown <mazenosama987654321@gmail.com>
2025-05-03 05:35:22 +0300

    settings

--- BehindTheSearch/behind_the_search/settings.py
+++ BehindTheSearch/behind_the_search/settings.py
@@ -139,3 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+LOGIN_REDIRECT_URL = 'video_list'
+LOGOUT_REDIRECT_URL = 'landing-page'
+
